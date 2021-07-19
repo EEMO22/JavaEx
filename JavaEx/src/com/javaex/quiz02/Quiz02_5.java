c38a19a4b0a326562c4400d9c2cbadeff82a8015
강호진 <battlemech@naver.com>
2021-07-19 18:49:03 +0900

    Quiz02_5 수정

--- JavaEx/src/com/javaex/quiz02/Quiz02_5.java
+++ JavaEx/src/com/javaex/quiz02/Quiz02_5.java
@@ -5,10 +5,19 @@ public class Quiz02_5 {
 		int num1 = 13579;
 		int num2 = 13579;
 		
+		System.out.println(++num1 + 1);
+		System.out.println(num2++ + 1);
+		
+		int num3 = (++num1 + 1);
+		int num4 = (num2++ + 1);
+		
+		System.out.println(num3);
+		System.out.println(num4);
+		
 		String result;
 		result = (++num1 + 1 == num2++ +1) ? "같음" : "다름";
 		
-		System.out.print("++num + 1 = " + ++num1 + 1+ "이고, " + "num2++ +1 = " + num2++ +1 + "이므로, " + result);
+		System.out.print("++num + 1 = " + num3 + "이고, " + "num2++ +1 = " + num4 + "이므로, " + result);
 	}
 
 }
