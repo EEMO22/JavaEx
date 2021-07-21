dfb6e692cf9eb66e3138a26f5b33fa0fc1e03997
강호진 <battlemech@naver.com>
2021-07-21 11:26:28 +0900

    String 참조 타입

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -11,9 +11,56 @@ public class LoopEx {
 //		forGugu();
 //		continueEx();
 //		loopPractice01();
-		loopPractice02();
+//		loopPractice02();
+//		whileGugupractice();
+//		forGuguPractice();
+//		whileStarPractice();
+//		forStarPractice();
+		breakEx();
 		System.out.println("End of Code");
 	}
+	
+		private static void breakEx() {
+			int num = 1;
+			while(true) {	//	무한 루프
+				if (num % 6 == 0 && num % 14 == 0)
+					break;	//	루프 탈출
+				num++;
+			}
+			System.out.println(num);
+		}
+	
+		// for * 그리기 < 선생님 > 
+
+		private static void forStarPractice() {
+			//	행 루프
+			for (int row = 1; row <= 6; row++) {
+				for (int col = 1; col <= row; col++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+
+		//	while * 그리기 < 선생님 >
+	
+		private static void whileStarPractice() {
+			int row = 1;
+			//	행 루프
+			while (row <= 6) {
+				//	열 루프
+				int col = 1;
+				while (col <= row) {
+					System.out.print("*");
+					col++;
+				}
+				System.out.println();	//	한 행 끝난 후 개행
+				row++;
+			}
+		}
+	
+		//	* 그리기 < 나 >
+		
 		private static void loopPractice02() {
 			for (int i = 1; i < 7; i++) {
 				for (int j = 0; j < i ; j++){
@@ -22,6 +69,8 @@ public class LoopEx {
 				System.out.println("");
 			}
 		}
+		
+		//	for문 구구단 <나>
 		private static void loopPractice01() {
 			
 			for (int i = 2; i <= 9; i++) {
@@ -31,6 +80,37 @@ public class LoopEx {
 				System.out.println("");
 			}
 		}
+		
+		//	for 구구단 <선생님>
+		
+		private static void forGuguPractice() {
+			//	단 루프
+			for (int dan = 2; dan <= 9; dan++) {
+				//	숫자 루프
+				for (int num = 1; num <=9; num++) {
+					System.out.println(dan + "*" + num + "=" + (dan * num));
+				}
+			}
+		}
+		
+		//	while 구구단 <선생님>
+		
+		private static void whileGugupractice() {
+			int dan = 2;
+			//	단 루프
+			while(dan <= 9) {
+				int num = 1;
+				//	숫자 루프
+				while(num <= 9) {
+					System.out.println(dan + "*" +num + "=" + (dan*num));
+					
+					num++;
+				}
+				dan++;
+			}
+			
+		}
+		
 		private static void continueEx() {
 			
 			for (int i = 1;
@@ -56,6 +136,7 @@ public class LoopEx {
 			
 			scanner.close();
 		}
+		
 		private static void doWhileEx() {
 			//	적어도 한 번은 수행되어야 하는 반복,
 			//	반복 조건이 반복문 내부에서 할당되는 경우
@@ -72,6 +153,7 @@ public class LoopEx {
 			System.out.println("합계: " + total);
 			scanner.close();
 		}
+		
 		private static void whileGugu() {
 			Scanner scanner = new Scanner(System.in);
 			int dan, i = 1;	//	반복 제어 변수
@@ -86,6 +168,7 @@ public class LoopEx {
 				i++;
 			}
 		}
+		
 		private static void whileEx() {
 			int num = 0;	//	반복 조건 제어 변수
 			
