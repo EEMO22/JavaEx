cc85a2eb6ee903427446571893be10cb252f6440
강호진 <battlemech@naver.com>
2021-07-22 14:45:29 +0900

    Method, Getter, Setter

--- JavaEx/src/com/javaex/oop/MethodEx.java
+++ JavaEx/src/com/javaex/oop/MethodEx.java
@@ -6,8 +6,45 @@ public class MethodEx {
 //		printMessage();
 //		printMessage("Hello, Method");	//	"Hello, Method" <- parameter
 //		printDivide(10, 0);	//	10, 0 <- parameter
-		System.out.println(divide(10, 3));	//	int 값인 10 과3 은 casting 되어 double
+//		System.out.println(divide(10, 3));	//	int 값인 10 과3 은 casting 되어 double
+//		System.out.println("getSum: " + 
+//				getSum(new double[] {
+//				1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } ));
+//		System.out.println("getSumVar: " + 
+//				getSumVar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		printSum("고정 + 가변: ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 	}
+	
+	//	고정 인수가 앞에, 가변 인수가 뒤에.
+	private static void printSum(String message, double ...values) {
+		System.out.println(message + ":" + getSumVar(values));
+	}
+	
+	//	매개 변수의 갯수를 모른다.
+	//	방법2. 가변 인수: 변수 이름 앞에 ...
+	private static double getSumVar(double ...values) {
+		//	가변인수는 배열로 변환되어 들어온다.
+//		double total = 0;
+//		
+//		for (double value: values) {
+//			total += value;
+//		}
+//		return total;
+		return getSum(values);
+	}
+	
+	//	매개 변수의 갯수를 모른다
+	//	방법1. 인수를 배열로 전달
+	private static double getSum(double[] values) {
+		double total = 0;
+		
+		for (double value: values) {
+			total += value;
+		}
+		
+		return total;
+	}
+	
 	//	입력 O, 출력 O
 	private static double divide(double num1, double num2) {
 		return num1 / num2;	//	double을 받겠다 했으니(double divide) 반드시 리턴 해야 함
@@ -18,7 +55,7 @@ public class MethodEx {
 		if (num2 == 0) {
 			System.out.println("0으로 나눌 수 없어요!");
 			return;	//	돌아가세요! : printDivide(10, 0); 로 돌아감
-					//	리턴 타입이 없더라도 return을 만나면 메서드 중단하고 돌아감
+					//	리턴 타입이 없더라도 return을 만나면 메서드 중단하고 main 함수로 돌아감
 		}
 		System.out.println(num1 / num2);
 		
--- JavaEx/src/com/javaex/oop/goods/v2/Goods.java
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -1,8 +1,32 @@
 package com.javaex.oop.goods.v2;
 
 //	v2. 접근 제한자
+//	getter/setter를 통한 내부 필드의 우회 접근
 public class Goods {
 	//	필드 선언
-	String name;
-	int price;
+	private String name;
+	private int price;
+	
+	//	getters/setters
+	public String getName() {
+		return name;		//	외부에서 요청하면 name 반환해 줌
+	}
+	
+	public void setName(String name) {
+		this.name = name;	//	외부에서 받은 name을 여기 자신의 name에 전달
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	//	메서드
+	public void showInfo() {
+		System.out.println("상품이름: " + name);
+		System.out.println("가격: " + price);
+	}
 }
